f297a89051c8588799adfb0bcff19ef043b1f17b
Brian Lee <lee@biltcard.com>
2025-04-07 23:31:09 -0400

    remove env var

--- src/app/api/apple/validate/route.ts
+++ src/app/api/apple/validate/route.ts
@@ -5,7 +5,7 @@ export const POST = async (request: Request) => {
     method: "POST",
     body: JSON.stringify({
       merchantIdentifier: process.env.APPLE_MERCHANT_IDENTIFIER,
-      displayName: process.env.APPLE_DISPLAY_NAME,
+      displayName: "Bilt Rewards",
       initiative: "web",
       initiativeContext: process.env.APPLE_DOMAIN_NAME,
     }),
