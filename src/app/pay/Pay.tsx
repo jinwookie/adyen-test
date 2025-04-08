7641409f6f2d6e531293f2dbc92844b9e1554d49
Brian Lee <lee@biltcard.com>
2025-04-07 23:59:08 -0400

    add console error

--- src/app/pay/Pay.tsx
+++ src/app/pay/Pay.tsx
@@ -86,6 +86,7 @@ const Pay = ({ paymentMethodsResponse }: Props) => {
             const data = await response.json();
             resolve(data);
           } catch (error) {
+            console.error(error);
             reject(error);
           }
         },
