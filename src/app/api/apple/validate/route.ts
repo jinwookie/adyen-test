c1bbf1fd462920ad181c3a3ef715a94fb22ee789
Brian Lee <lee@biltcard.com>
2025-04-07 23:48:41 -0400

    add error handling

--- src/app/api/apple/validate/route.ts
+++ src/app/api/apple/validate/route.ts
@@ -11,5 +11,23 @@ export const POST = async (request: Request) => {
     }),
   });
 
-  return response;
+  console.log(response);
+  if (!response.ok) {
+    const error = await response.json();
+    console.error(error);
+    return Response.json(error, {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  const data = await response.json();
+  console.log(data);
+  return Response.json(data, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
